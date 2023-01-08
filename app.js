e426856bd8fef3eb099ddfd96ec7d83adc627d4d
Abhiram Mithur <abhiram.mithur@gmail.com>
2023-01-08 14:13:24 +0530

    completed app

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-var btnTranslate= document.querySelector("#btn-translate");
-var txtInput= document.querySelector("#txt-input");
-var outputDiv= document.querySelector("#output");   
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler(){
-   outputDiv.innerText="alalalalal "+ txtInput.value;
+function getTranslatedURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("Something wrong at server.Try again later")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+
+        .catch(errorHandler)
 };
 
-btnTranslate.addEventListener("click",clickHandler)
+btnTranslate.addEventListener("click", clickHandler)
